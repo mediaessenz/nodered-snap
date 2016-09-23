3c50aeaf146cb616b6ff30acafe71422b20ee65f
Maarten Ectors <maarten@ectors.com>
2016-09-23 19:55:00 +0100

    fixed nodejs bug workaround

--- build_snap.sh
+++ build_snap.sh
@@ -7,11 +7,7 @@ sudo apt update
 sudo apt -y install g++ make python libbluetooth-dev libudev-dev
 #cd $SNAPSRC
 snapcraft clean
-# bug in prime for nodejs plugin remove section when fixed
-snapcraft prime
-cp -R stage/lib/node_modules prime/lib/node_modules
-# end bug workaround
-snapcraft snap
+snapcraft
 
 #cd $SNAPPYVM
 #scp  -P 2222 -i .vagrant/machines/default/virtualbox/private_key  $SNAPSRC/$SNAPNAME ubuntu@127.0.0.1:~/
