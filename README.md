92f53644f791d40bc98179164d87c47e8cf4199e
Dave Conway-Jones <conway@uk.ibm.com>
2017-01-10 20:08:51 +0000

    clean up

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-# nodered.snap
-Node  RED snappy package
+# node-red.snap
 
-To build, execute build_snap.sh
+### Node-RED snap package
 
-Listens on port 1880
+Base version of the Node-RED internet of things graphical wiring tool packaged as an
+Ubuntu Snap, intended for multiple architectures.
+
+Listens on port 1880 by default
+
+#### Building
+
+To build, execute `build_snap.sh`
+
+#### Installing
+
+    sudo snap install node-red
+
+When the snap is running you can view the system log using
+
+    tail -f /var/log/syslog | grep ' snap'
+
+You may wish to filter it further of course if you run a lot of snaps.
--- build_snap.sh
+++ build_snap.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-#SNAPPYVM=../snappy-vm
-#SNAPSRC=`pwd`
-#SNAPNAME=node-red_1.0_amd64.snap
 sudo apt update
-sudo apt -y install g++ make python libbluetooth-dev libudev-dev
-#cd $SNAPSRC
+sudo apt -y install g++ make python libbluetooth-dev libudev-dev snapd snapcraft
+
 snapcraft clean
 snapcraft
-
-#cd $SNAPPYVM
-#scp  -P 2222 -i .vagrant/machines/default/virtualbox/private_key  $SNAPSRC/$SNAPNAME ubuntu@127.0.0.1:~/
-#vagrant ssh -c "sudo snappy install $SNAPNAME --allow-unauthenticated"
-
-#snappy-remote --pub-key=/home/l/git/create-net/agile/snappy-vm/.vagrant/machines/default/virtualbox/private_key --url=ubuntu@127.0.0.1:2222 install $SNAPSRC/*.snap
