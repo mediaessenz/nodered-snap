d2639f4d19afb57e0f3b1568d09bb5a37dc07673
Dave Conway-Jones <conway@uk.ibm.com>
2018-08-29 16:55:01 +0100

    snap 0.19.2

--- build_snap.sh
+++ build_snap.sh
@@ -5,4 +5,4 @@ sudo apt -y install g++ make python libbluetooth-dev libudev-dev snapd snapcraft
 
 snapcraft clean
 snapcraft
-snapcraft release node-red 0.19.1 stable
+snapcraft release node-red 0.19.2 stable
