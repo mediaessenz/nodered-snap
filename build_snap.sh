de959e45126039d22e39c9a32613c2e576cda9c2
Dave Conway-Jones <conway@uk.ibm.com>
2018-09-03 13:55:41 +0100

    add libc6

--- build_snap.sh
+++ build_snap.sh
@@ -3,5 +3,6 @@
 # sudo apt update
 # sudo apt -y install g++ make python libbluetooth-dev libudev-dev snapd snapcraft
 
-snapcraft cleanbuild --debug
+snapcraft clean
+snapcraft --debug
 snapcraft release node-red 0.19.2 stable
